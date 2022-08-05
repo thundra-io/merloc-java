ea65a17466e1dd6d70d8a0b3e0c64d969b2782a3
Serkan ÖZAL <serkan@thundra.io>
2022-08-05 12:19:58 +0300

    Update `release-layer.sh` for releasing to all regions

--- scripts/release-layer.sh
+++ scripts/release-layer.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 LAYER_NAME="merloc-java"
-#REGIONS=( "ap-northeast-1" "ap-northeast-2" "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ap-east-1" "ca-central-1" "eu-central-1" "eu-north-1" "eu-south-1" "eu-west-1" "eu-west-2" "eu-west-3" "sa-east-1" "us-east-1" "us-east-2" "us-west-1" "us-west-2" )
-REGIONS=( "us-west-2" )
+REGIONS=( "ap-northeast-1" "ap-northeast-2" "ap-south-1" "ap-southeast-1" "ap-southeast-2" "ap-east-1" "ca-central-1" "eu-central-1" "eu-north-1" "eu-south-1" "eu-west-1" "eu-west-2" "eu-west-3" "sa-east-1" "us-east-1" "us-east-2" "us-west-1" "us-west-2" )
 WORKSPACE_DIR="workspace"
 STATEMENT_ID_BASE="$LAYER_NAME-$(($(date +%s)))"
 
